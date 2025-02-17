3b8d459662f1f652bd67ec1198e48531e4ff06b5
dinithaw <ldinitha@gmail.com>
2025-02-18 01:50:43 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Instagram-Unfollowers
-Find who doesn't follow you back on instagram!
+# Instagram Unfollowers Checker
+
+This project helps you find out who doesn't follow you back on Instagram by comparing your list of followers with the list of people you are following.
+
+## Features
+
+- Upload JSON files containing your Instagram followers and following lists.
+- Identify users who do not follow you back.
+- Display the list of unfollowers in a user-friendly interface.
+
+## Getting Started
+
+### Prerequisites
+
+To use this project, you need:
+
+- A web browser (Chrome, Firefox, Safari, etc.)
+- JSON files containing your Instagram followers and following lists. You can obtain these files by requesting your data from Instagram.
--- script.js
+++ script.js
@@ -1,39 +1,84 @@
-function readFile(file, callback) {
-    const reader = new FileReader();
-    reader.onload = (event) => callback(JSON.parse(event.target.result));
-    reader.readAsText(file);
-}
+// function readFile(file, callback) {
+//     const reader = new FileReader();
+//     reader.onload = (event) => callback(JSON.parse(event.target.result));
+//     reader.readAsText(file);
+// }
 
-function extractUsernames(data) {
-    return new Set(data.relationships_following.map(item => item.string_list_data[0].value));
-}
+// function extractUsernames(data) {
+//     return new Set(data.relationships_following.map(item => item.string_list_data[0].value));
+// }
 
-function checkUnfollowers() {
-    const followingFile = document.getElementById("following").files[0];
-    const followersFile = document.getElementById("followers").files[0];
+// function checkUnfollowers() {
+//     const followingFile = document.getElementById("following").files[0];
+//     const followersFile = document.getElementById("followers").files[0];
 
-    if (!followingFile || !followersFile) {
-        alert("Please upload both files!");
-        return;
+//     if (!followingFile || !followersFile) {
+//         alert("Please upload both files!");
+//         return;
+//     }
+
+//     readFile(followingFile, (followingData) => {
+//         readFile(followersFile, (followersData) => {
+//             const following = extractUsernames(followingData);
+//             const followers = extractUsernames(followersData);
+
+//             const unfollowers = [...following].filter(user => !followers.has(user));
+//             displayUnfollowers(unfollowers);
+//         });
+//     });
+// }
+
+// function displayUnfollowers(unfollowers) {
+//     const list = document.getElementById("unfollowersList");
+//     list.innerHTML = "";
+//     unfollowers.forEach(user => {
+//         const li = document.createElement("li");
+//         li.textContent = user;
+//         list.appendChild(li);
+//     });
+// }
+
+document.getElementById('checkButton').addEventListener('click', function() {
+    const followingFile = document.getElementById('followingFile').files[0];
+    const followersFile = document.getElementById('followersFile').files[0];
+
+    if (followingFile && followersFile) {
+        const followingReader = new FileReader();
+        const followersReader = new FileReader();
+
+        followingReader.onload = function(event) {
+            const followingList = JSON.parse(event.target.result).relationships_following;
+            followersReader.onload = function(event) {
+                const followersList = JSON.parse(event.target.result);
+                const unfollowers = findUnfollowers(followingList, followersList);
+                displayUnfollowers(unfollowers);
+            };
+            followersReader.readAsText(followersFile);
+        };
+        followingReader.readAsText(followingFile);
+    } else {
+        alert('Please upload both files.');
     }
+});
 
-    readFile(followingFile, (followingData) => {
-        readFile(followersFile, (followersData) => {
-            const following = extractUsernames(followingData);
-            const followers = extractUsernames(followersData);
-
-            const unfollowers = [...following].filter(user => !followers.has(user));
-            displayUnfollowers(unfollowers);
-        });
-    });
+function findUnfollowers(followingList, followersList) {
+    const followingSet = new Set(followingList.map(user => user.string_list_data[0]?.value));
+    const followersSet = new Set(followersList.map(user => user.string_list_data[0]?.value));
+    return [...followingSet].filter(username => username && !followersSet.has(username));
 }
 
 function displayUnfollowers(unfollowers) {
-    const list = document.getElementById("unfollowersList");
-    list.innerHTML = "";
-    unfollowers.forEach(user => {
-        const li = document.createElement("li");
-        li.textContent = user;
-        list.appendChild(li);
-    });
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerHTML = '';
+    if (unfollowers.length === 0) {
+        resultContainer.innerHTML = '<p>All users follow you back!</p>';
+    } else {
+        const ul = document.createElement('ul');
+        unfollowers.forEach(username => {
+            const li = document.createElement('li');
+            li.textContent = username;
+            ul.appendChild(li);
+        });
+        resultContainer.appendChild(ul);
+    }
 }
